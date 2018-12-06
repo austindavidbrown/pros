126da98f810a8fc9788bdf8b7310d2afe99d3ffb
Austin David Brown <modparadigm@gmail.com>
2018-12-05 22:27:02 -0600

    presentation

--- analysis.R
+++ analysis.R
@@ -3,7 +3,6 @@ library(pros)
 library(glmnet)
 
 d = data.matrix(read.table("http://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/prostate.data"))
-head(d)
 train = d[d[, "train"] == TRUE, ]
 test = d[d[, "train"] == FALSE, ]
 
@@ -13,34 +12,86 @@ y_train = as.vector(train[, 9])
 X_test = as.matrix(test[, 1:8])
 y_test = as.vector(test[, 9])
 
-fit_glmnet = glmnet(X_train, cy_train, alpha = 1, lambda = .1, intercept = T, standardize = F)
-mean((y_test - predict(fit_glmnet, X_test))^2)
+X_train = scale(X_train)
+X_test = scale(X_test)
 
-cv_glmnet = cv.glmnet(nX_train, cy_train, alpha = 1, standardize = F)
-coef(cv_glmnet)
+cv_glmnet_lasso = cv.glmnet(X_train, y_train, alpha = 1, nfolds = 10)
+mean((y_test - predict(cv_glmnet_lasso, X_test))^2)
+
+cv_glmnet = cv.glmnet(X_train, y_train, alpha = 1/2, nfolds = 10, standardize = T)
 mean((y_test - predict(cv_glmnet, X_test))^2)
 
-fit = pros(X_train, y_train, lambda = 0.01456746, max_iter = 100000, tolerance = 10^(-2))
-fit
+###
+# BEATS GLMNET
+###
+alpha = c(.2, 0, 0, 0, 0, 1 -.2, 0)
+fit = pros(X_train, y_train, lambda = 80, alpha = alpha, max_iter = 100000, tolerance = 10^(-6))
 mean((y_test - predict(fit, X_test))^2)
 
-cv_pros = cv.pros(cX_train, cy_train, max_iter = 100000, tolerance = 10^(-3))
+
+###
+# BEATS GLMNET
+###
+alpha = c(1/3, 0, 0, 0, 0, 2/3, 0)
+fit = pros(X_train, y_train, lambda = 30, alpha = alpha, max_iter = 100000, tolerance = 10^(-4))
+mean((y_test - predict(fit, X_test))^2)
+
+
+###
+# BEATS GLMNET
+###
+alpha = c(1/5, 0, 1/5, 1/5, 1/5, 1/5, 0)
+fit = pros(X_train, y_train, lambda = 50, alpha = alpha, max_iter = 100000, tolerance = 10^(-4))
+mean((y_test - predict(fit, X_test))^2)
+
+###
+# BEATS GLMNET
+###
+alpha = c(1/2, 0, 0, 0, 0, 1/2, 0)
+fit = pros(X_train, y_train, lambda = 19, alpha = alpha, max_iter = 100000, tolerance = 10^(-5))
+mean((y_test - predict(fit, X_test))^2)
+
+###
+# BEATS GLMNET
+###
+alpha = c(1/5, 0, 1/5, 1/5, 1/5, 1/5, 0)
+fit = pros(X_train, y_train, lambda = 58, alpha = alpha, max_iter = 100000, tolerance = 10^(-3))
+mean((y_test - predict(fit, X_test))^2)
+
+###
+# BEST SO FAR
+###
+alpha = c(1/5, 0, 1/5, 1/5, 1/5, 1/5, 0)
+fit = pros(X_train, y_train, lambda = 58, alpha = alpha, max_iter = 100000, tolerance = 10^(-3))
+mean((y_test - predict(fit, X_test))^2)
+
+lambdas = seq(100, 110, .1)
+cv_pros = cv.pros(X_train, y_train, alpha = alpha, lambdas = lambdas, K_fold = 5, max_iter = 200000, tolerance = 10^(-3))
 mean((y_test - predict(cv_pros, X_test))^2)
 
-best_fit = pros(cX_train, cy_train, lambda = cv_pros$best_lambda, max_iter = 100000, tolerance = 10^(-3))
+
+
+
+###
+# BEST SO FAR
+###
+alpha = c(1/3, 0, 1/3, 1/3, 0, 0, 0)
+fit = pros(X_train, y_train, lambda = 25, alpha = alpha, max_iter = 100000, tolerance = 10^(-3))
+mean((y_test - predict(fit, X_test))^2)
+
+lambdas = seq(100, 110, .1)
+cv_pros = cv.pros(X_train, y_train, alpha = alpha, lambdas = lambdas, K_fold = 5, max_iter = 200000, tolerance = 10^(-3))
+mean((y_test - predict(cv_pros, X_test))^2)
+
+
+###
+# BEST SO FAR
+###
+alpha = c(1 - .26, 0, .26, 0, 0, 0, 0)
+lambdas = seq(10^(-7), 10, .1)
+cv_pros = cv.pros(X_train, y_train, alpha = alpha, lambdas = lambdas, K_fold = 10, max_iter = 200000, tolerance = 10^(-3))
+mean((y_test - predict(cv_pros, X_test))^2)
+
+
+best_fit = pros(X_train, y_train, alpha = alpha, lambda = cv_pros$best_lambda, max_iter = 100000, tolerance = 10^(-3))
 mean((y_test - predict(best_fit, X_test))^2)
-
-
-
-
-
-### Graveyard
-
-nX_train = scale(X_train)
-nX_test = scale(X_test)
-cy_train = y_train - rep(mean(y_train), length(y_train))
-
-# Center
-#C = diag(1, nrow(X_train)) - 1/nrow(X_train) * rep(1, nrow(X_train)) %*% t(rep(1, nrow(X_train)))
-#cX_train = C %*% X_train
-#cy_train = y_train - rep(mean(y_train), length(y_train))
