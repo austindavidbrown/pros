99a31afa4c96b634e1e7ae219b2f6cf524f7f2f8
Austin David Brown <modparadigm@gmail.com>
2018-12-21 20:35:52 -0600

    fixes

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
 
-# Testing
-default:
+develop:
 	clang++ -Wall -Wextra -std=c++17 -I ./eigen/ pros_test.cpp -O3 -march=native -o pros_test
 	./pros_test
 
-# Production
 production:
 	clang++ -std=c++17 -I ./eigen/ test.cpp -o test -DNDEBUG -O3 -march=native -mfpmath=sse && ./test
+
+build_R:
+	R -f R_build.R
+
+build_python:
+	pip install ./python-package
+
+
+
+
+
 
 
 # References:
--- R_build.R
+++ R_build.R
@@ -1,33 +1,16 @@
 # Build registration
 tools::package_native_routine_registration_skeleton("./R-package", "R-package/src/init.c")
 
-# Generate reference manual
-R CMD Rd2pdf ./R-package --force -o pros-manual.pdf
-
 # Document
 R CMD INSTALL ./R-package # needs to install package to document
 devtools::document("./R-package")
-roxygen2::roxygenise("./R-package")
+# roxygen2::roxygenise("./R-package")
+
+# Generate reference manual
+system("R CMD Rd2pdf ./R-package --force -o pros-manual.pdf")
 
 # Check
-R CMD check --as-cran ./R-package
+system("R CMD check --as-cran ./R-package")
 devtools::check("./R-package")
 
-##
-# Test installation
-##
 devtools::install("R-package")
-library("pros")
-
-X_train = read.csv("data/X_train.csv", header = F)
-y_train = read.csv("data/y_train.csv", header = F)
-
-X_test = read.csv("data/X_test.csv", header = F)
-y_test = read.csv("data/y_test.csv", header = F)
-
-fit = pros(X_train, y_train, lambda = .1, step_size = .001)
-pred = predict(fit, X_test)
-pred
-cv = cv.pros(X_train, y_train, step_size = .001)
-pred = predict(cv, X_test)
-pred
